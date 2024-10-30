3ac739b7b0e1c3cf316d1e7333228509ec8f0fbc
Faisal Kanout <faisal@kanout.com>
2024-10-30 17:51:59 +0100

    Add restaurants

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,18 @@ import { pushFileToGitHub } from "./helpers/github";
 
 (async () => {
 
-  const restaurant = await constructRestaurant(RESTAURANTS_URL.TAZEH_KASAP_KAYA, RESTAURANTS_IDS.TAZEH_KASAP_KAYA);
+  const restaurant_TAZEH_KASAP_KAYA = await constructRestaurant(RESTAURANTS_URL.TAZEH_KASAP_KAYA, RESTAURANTS_IDS.TAZEH_KASAP_KAYA);
+  pushFileToGitHub(restaurant_TAZEH_KASAP_KAYA, restaurant_TAZEH_KASAP_KAYA.id);
 
-  pushFileToGitHub(restaurant, restaurant.id)
+  const restaurant_BLUDAN_KAYA = await constructRestaurant(RESTAURANTS_URL.BLUDAN_KAYA, RESTAURANTS_IDS.BLUDAN_KAYA);
+  pushFileToGitHub(restaurant_BLUDAN_KAYA, restaurant_BLUDAN_KAYA.id);
+
+  const restaurant_FALAFEL_SARAYI = await constructRestaurant(RESTAURANTS_URL.FALAFEL_SARAYI, RESTAURANTS_IDS.FALAFEL_SARAYI);
+  pushFileToGitHub(restaurant_FALAFEL_SARAYI, restaurant_FALAFEL_SARAYI.id);
+
+  const restaurant_KARADISH = await constructRestaurant(RESTAURANTS_URL.KARADISH, RESTAURANTS_IDS.KARADISH);
+  pushFileToGitHub(restaurant_KARADISH, restaurant_KARADISH.id);
+
+  const restaurant_HAWA_MAHAL = await constructRestaurant(RESTAURANTS_URL.HAWA_MAHAL, RESTAURANTS_IDS.HAWA_MAHAL);
+  pushFileToGitHub(restaurant_HAWA_MAHAL, restaurant_HAWA_MAHAL.id);
 })()
