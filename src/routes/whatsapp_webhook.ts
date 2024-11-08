e8a6f075c1de578613ac96b5884243efeb074399
Faisal Kanout <faisal@kanout.com>
2024-11-08 18:36:54 +0100

    add read

--- src/routes/whatsapp_webhook.ts
+++ src/routes/whatsapp_webhook.ts
@@ -1,14 +1,34 @@
+import axios from "axios";
 import { FastifyInstance } from "fastify";
 export default async function whatsappWebhookRoute(server: FastifyInstance) {
-    server.post('/whatsapp_webhook', { preHandler: server.whatsAppHook }, async (request, reply) => {
-        const query = request.query as Record<string, string>
-        console.log(request.body)
-        reply.code(200).send(query['hub.challenge'])
-    })
-
     server.get('/whatsapp_webhook', { preHandler: server.whatsAppHook }, async (request, reply) => {
         const query = request.query as Record<string, string>
         console.log(request.body)
         reply.code(200).send(query['hub.challenge'])
     })
+
+    server.post('/whatsapp_webhook', async (request, reply) => {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        const body = request.body as Record<string, any>
+        console.log(request.body)
+        const business_phone_number_id =
+            body.entry?.[0].changes?.[0].value?.metadata?.phone_number_id;
+        const message = body.entry?.[0]?.changes[0]?.value?.messages?.[0];
+
+        await axios({
+            method: "POST",
+            url: `https://graph.facebook.com/v20.0/${business_phone_number_id}/messages`,
+            headers: {
+                Authorization: `Bearer ${process.env.GRAPH_API_TOKEN}`,
+            },
+            data: {
+                messaging_product: "whatsapp",
+                status: "read",
+                message_id: message.id,
+            },
+        });
+        reply.code(200);
+    })
+
+
 }
