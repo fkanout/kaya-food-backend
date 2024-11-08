e4209ac124ea5589fdeaaea3d5086561e71a9d48
Faisal Kanout <faisal@kanout.com>
2024-11-08 17:33:03 +0100

    add query

--- src/routes/whatsapp_webhook.ts
+++ src/routes/whatsapp_webhook.ts
@@ -2,7 +2,7 @@ import { FastifyInstance } from "fastify";
 export default async function whatsappWebhookRoute(server: FastifyInstance) {
     server.get('/whatsapp_webhook', { preHandler: server.whatsAppHook }, async (request, reply) => {
         reply.send({
-            ...request.headers
+            query: request.query
         })
     })
 }
