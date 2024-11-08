08c68615c1f063630bc45628670c24b547660efc
Faisal Kanout <faisal@kanout.com>
2024-11-08 17:59:10 +0100

    fix

--- src/routes/decorators/whatsapp.ts
+++ src/routes/decorators/whatsapp.ts
@@ -5,7 +5,7 @@ export const registerWhatsappHookDecorator = (server: FastifyInstance) => {
         server.decorate('whatsAppHook', async (request: FastifyRequest, reply: FastifyReply) => {
             try {
                 const query = request.query as Record<string, string>;
-                if (query['hub.verify_token'] === process.env.WHATSAPP_HOOK_VERIFY_TOKEN || "") {
+                if (query['hub.verify_token'] !== process.env.WHATSAPP_HOOK_VERIFY_TOKEN || "") {
                     throw ("VERIFY TOKEN is not correct")
                 }
                 // eslint-disable-next-line @typescript-eslint/no-unused-vars
