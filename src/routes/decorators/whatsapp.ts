da717180a119d5c3feea9ad268bbc3103803c75e
Faisal Kanout <faisal@kanout.com>
2024-11-08 17:55:51 +0100

    fix

--- src/routes/decorators/whatsapp.ts
+++ src/routes/decorators/whatsapp.ts
@@ -4,9 +4,8 @@ export const registerWhatsappHookDecorator = (server: FastifyInstance) => {
     const authDecorator: FastifyPluginAsync = async () => {
         server.decorate('whatsAppHook', async (request: FastifyRequest, reply: FastifyReply) => {
             try {
-                // eslint-disable-next-line @typescript-eslint/no-explicit-any
-                const query = request.query as Record<string, any>;
-                if (query?.hub?.verify_token === process.env.WHATSAPP_HOOK_VERIFY_TOKEN || "") {
+                const query = request.query as Record<string, string>;
+                if (query['hub.verify_token'] === process.env.WHATSAPP_HOOK_VERIFY_TOKEN || "") {
                     throw ("VERIFY TOKEN is not correct")
                 }
                 // eslint-disable-next-line @typescript-eslint/no-unused-vars
--- src/routes/whatsapp_webhook.ts
+++ src/routes/whatsapp_webhook.ts
@@ -1,8 +1,7 @@
 import { FastifyInstance } from "fastify";
 export default async function whatsappWebhookRoute(server: FastifyInstance) {
     server.get('/whatsapp_webhook', { preHandler: server.whatsAppHook }, async (request, reply) => {
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        const query = request.query as Record<string, any>
-        reply.send(query.hub.challenge)
+        const query = request.query as Record<string, string>
+        reply.send(query['hub.challenge'])
     })
 }
