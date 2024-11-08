ef03f7f0ba2da999bec7e7241eacc4b29f17a30f
Faisal Kanout <faisal@kanout.com>
2024-11-08 10:33:37 +0100

    fixes

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+require('dotenv').config()
+
 // import { startCrawler } from "./crawler"
 import { startServer } from "./server"
 // startCrawler()
