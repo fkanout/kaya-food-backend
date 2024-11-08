02eada08d20045991086106b76310942f53a2a35
Faisal Kanout <faisal@kanout.com>
2024-11-08 17:35:38 +0100

    log

--- src/routes/decorators/whatsapp.ts
+++ src/routes/decorators/whatsapp.ts
@@ -4,7 +4,7 @@ export const registerWhatsappHookDecorator = (server: FastifyInstance) => {
     const authDecorator: FastifyPluginAsync = async () => {
         server.decorate('whatsAppHook', async (request: FastifyRequest, reply: FastifyReply) => {
             try {
-                const authorization = request.headers;
+                const authorization = request.query;
                 console.log(authorization)
                 // eslint-disable-next-line @typescript-eslint/no-unused-vars
             } catch (error: unknown) {
