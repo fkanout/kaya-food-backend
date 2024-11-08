15a98eda05baccb597cf63f9b49387c190f8ccd7
Faisal Kanout <faisal@kanout.com>
2024-11-08 18:11:12 +0100

    add post

--- src/routes/whatsapp_webhook.ts
+++ src/routes/whatsapp_webhook.ts
@@ -3,6 +3,12 @@ export default async function whatsappWebhookRoute(server: FastifyInstance) {
     server.post('/whatsapp_webhook', { preHandler: server.whatsAppHook }, async (request, reply) => {
         const query = request.query as Record<string, string>
         console.log(request.body)
-        reply.send(query['hub.challenge'])
+        reply.code(200).send(query['hub.challenge'])
+    })
+
+    server.get('/whatsapp_webhook', { preHandler: server.whatsAppHook }, async (request, reply) => {
+        const query = request.query as Record<string, string>
+        console.log(request.body)
+        reply.code(200).send(query['hub.challenge'])
     })
 }
